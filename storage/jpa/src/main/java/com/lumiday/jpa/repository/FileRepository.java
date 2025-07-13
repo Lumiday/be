00891b5e3ce05d584e4b430a1456dd5eb96c249a
rappire <rappire@gmail.com>
2025-07-13 18:31:50 +0900

    feat: add @Repository annotation to repository interfaces for Spring component scanning

--- storage/jpa/src/main/java/com/lumiday/jpa/repository/FileRepository.java
+++ storage/jpa/src/main/java/com/lumiday/jpa/repository/FileRepository.java
@@ -3,7 +3,9 @@ package com.lumiday.jpa.repository;
 import com.lumiday.jpa.entity.FileEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileRepository extends JpaRepository<FileEntity, String> {
 
     Optional<FileEntity> findBySavedFileName(String imageName);
--- storage/jpa/src/main/java/com/lumiday/jpa/repository/InvitationRepository.java
+++ storage/jpa/src/main/java/com/lumiday/jpa/repository/InvitationRepository.java
@@ -2,6 +2,8 @@ package com.lumiday.jpa.repository;
 
 import com.lumiday.jpa.entity.InvitationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvitationRepository extends JpaRepository<InvitationEntity, String> {
 }
